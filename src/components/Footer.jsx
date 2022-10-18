4279e2da59e149b8993aab0375b2d0fb336d4ce5
Diego Almeida <diegoalmeidaz@gmail.com>
2022-10-18 18:44:17 -0300

    fix: cart format fixing, responsiveness aquaried

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-      <footer className="bg-info text-white p-3 mt-4 text-center font-semibold">Mamma Mia SPA</footer>
+      <footer className="bg-info text-white p-3 mt-4 text-center font-semibold ">Mamma Mia SPA</footer>
     );
   }
   
